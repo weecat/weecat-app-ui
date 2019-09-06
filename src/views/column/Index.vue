d6cfface71d8dd33cf27e369ec5b2523ecede45e
wangko27 <845885222@qq.com>
2019-09-06 17:16:51 +0800

    专栏数据模拟测试

--- src/views/column/Index.vue
+++ src/views/column/Index.vue
@@ -5,17 +5,17 @@
         <div class="aw-content-wrap clearfix simpobox">
           <div class="N-left clearfix">
             <div class="N-mytitle">
-              <h2><img src="http://wenda.wecenter.com/static/css/default/img/np.png">专栏</h2>
+              <h2><img src="@/assets/css/default/img/np.png">专栏</h2>
               <ul>
                 <li class="cur clearfix">
-                  <a href="http://wenda.wecenter.com/column/ajax/list/#column" data-sort="sum">默认</a>
+                  <a href="/column/ajax/list/#column" data-sort="sum">默认</a>
                 </li>
                 <li class="clearfix">
-                  <a href="http://wenda.wecenter.com/column/ajax/list/sort-new#column"
+                  <a href="/column/ajax/list/sort-new#column"
                      data-sort="new">最新</a>
                 </li>
                 <li class="clearfix">
-                  <a href="http://wenda.wecenter.com/column/ajax/list/sort-hot#column"
+                  <a href="/column/ajax/list/sort-hot#column"
                      data-sort="hot">热门</a>
                 </li>
                 <li class="clearfix">
@@ -25,244 +25,58 @@
             </div>
 
             <div class="N-zllist clearfix">
-
-              <div class="N-myzilistbox clearfix" id="column">
-                <dl>
-                  <dt>
-                    <img class="N-zimg" onclick="window.location = G_BASE_URL + '/column/details/9'"
-                         src="http://wenda.wecenter.com/uploads/column/daa68959d7fba4e7dff3197c7ac32e02.jpg?1266"
-                         style="cursor: pointer;">
-                    <a onclick="focus_column($(this), 9);">
-                      <img alt="关注" src="http://wenda.wecenter.com/static/css/default/img/nadd.png">
-
-                    </a>
-                  </dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/column/details/9">模版</a></h2>
-                    <p>模版收集与展示</p>
-                    <span>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 0</em>
-                </span>
-                    <span>
-                    <em>浏览量:0</em>
-                    <em>文章数:0</em>
-                </span>
-                  </dd>
-                </dl>
-                <dl style="float: right;">
-                  <dt>
-                    <img class="N-zimg" onclick="window.location = G_BASE_URL + '/column/details/14'"
-                         src="http://wenda.wecenter.com/uploads/column/338c244689f56b878dd16b0c764a96f1.jpg?137"
-                         style="cursor: pointer;">
-                    <a onclick="focus_column($(this), 14);">
-                      <img alt="关注" src="http://wenda.wecenter.com/static/css/default/img/nadd.png">
-
-                    </a>
-                  </dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/column/details/14">小柯SEO</a></h2>
-                    <p>分享网站SEO方面相关内容。10年SEO...</p>
-                    <span>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 0</em>
-                </span>
-                    <span>
-                    <em>浏览量:0</em>
-                    <em>文章数:0</em>
-                </span>
-                  </dd>
-                </dl>
-                <dl>
-                  <dt>
-                    <img class="N-zimg" onclick="window.location = G_BASE_URL + '/column/details/15'"
-                         src="http://wenda.wecenter.com/uploads/column/15907b9f669e22db7895bcc89a026080.jpg?9976"
-                         style="cursor: pointer;">
-                    <a onclick="focus_column($(this), 15);">
-                      <img alt="关注" src="http://wenda.wecenter.com/static/css/default/img/nadd.png">
-
-                    </a>
-                  </dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/column/details/15">去查网站目录</a></h2>
-                    <p>去查网站目录 解决网站收录问题 http...</p>
-                    <span>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 0</em>
-                </span>
-                    <span>
-                    <em>浏览量:0</em>
-                    <em>文章数:0</em>
-                </span>
-                  </dd>
-                </dl>
-                <dl style="float: right;">
-                  <dt>
-                    <img class="N-zimg" onclick="window.location = G_BASE_URL + '/column/details/16'"
-                         src="http://wenda.wecenter.com/uploads/column/dbf9e6ab838faceb16258f347c4391cc.png?866"
-                         style="cursor: pointer;">
-                    <a onclick="focus_column($(this), 16);">
-                      <img alt="关注" src="http://wenda.wecenter.com/static/css/default/img/nadd.png">
-
-                    </a>
-                  </dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/column/details/16">连接家人连接世界</a></h2>
-                    <p>欢迎来互联网讨论区</p>
-                    <span>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 0</em>
-                </span>
-                    <span>
-                    <em>浏览量:0</em>
-                    <em>文章数:0</em>
-                </span>
-                  </dd>
-                </dl>
-                <dl>
-                  <dt>
-                    <img class="N-zimg" onclick="window.location = G_BASE_URL + '/column/details/19'"
-                         src="http://wenda.wecenter.com/uploads/column/5b2ef2734bc6a0cc46926ec137b423e0.jpg?6335"
-                         style="cursor: pointer;">
-                    <a onclick="focus_column($(this), 19);">
-                      <img alt="关注" src="http://wenda.wecenter.com/static/css/default/img/nadd.png">
-
-                    </a>
-                  </dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/column/details/19">明语笔记</a></h2>
-                    <p>小明明的开发笔记。</p>
-                    <span>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 0</em>
-                </span>
-                    <span>
-                    <em>浏览量:0</em>
-                    <em>文章数:0</em>
-                </span>
-                  </dd>
-                </dl>
-                <dl style="float: right;">
-                  <dt>
-                    <img class="N-zimg" onclick="window.location = G_BASE_URL + '/column/details/21'"
-                         src="http://wenda.wecenter.com/uploads/column/00adc5e30f72c8958a9df2674eea71f9.jpg?5778"
-                         style="cursor: pointer;">
-                    <a onclick="focus_column($(this), 21);">
-                      <img alt="关注" src="http://wenda.wecenter.com/static/css/default/img/nadd.png">
-
-                    </a>
-                  </dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/column/details/21">快网插件</a></h2>
-                    <p>针对WC开放插件市场，提供一些成功的插件...</p>
-                    <span>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                    <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 0</em>
-                </span>
-                    <span>
-                    <em>浏览量:0</em>
-                    <em>文章数:0</em>
-                </span>
-                  </dd>
-                </dl>
+              <div class="N-myzilistbox clearfix">
+                <template v-for="column in columns">
+                  <dl v-bind:key="column.column_id">
+                    <dt>
+                      <img class="N-zimg" :src="'http://wenda.wecenter.com'+column.column_pic" style="cursor: pointer;">
+                      <a href="#">
+                        <img alt="关注" src="@/assets/css/default/img/nadd.png">
+                      </a>
+                    </dt>
+                    <dd>
+                      <h2>
+                        <a href="/column/details/9">{{column.column_name}}</a>
+                      </h2>
+                      <p>{{column.column_description}}</p>
+                      <span>
+                        <em><i><img src="@/assets/css/default/img/nz.png"></i> {{column.votes_count}}</em>
+                        <em><i><img src="@/assets/css/default/img/nsee.png"></i> {{column.views_count}}</em>
+                      </span>
+                      <span>
+                      <em>浏览量:{{column.views_count}}</em>
+                      <em>文章数:{{column.article_count}}</em>
+                    </span>
+                    </dd>
+                  </dl>
+                </template>
               </div>
-
               <h6 class="nhref" id="bp_all_more" data-page="1"><i>
                 <img src="http://wenda.wecenter.com/static/css/default/img/href.png"></i>换一换
               </h6>
               <div class="N-mytitles">
-                <h2><img src="http://wenda.wecenter.com/static/css/default/img/np.png">文章</h2>
+                <h2><img src="@/assets/css/default/img/np.png">文章</h2>
                 <a href="http://wenda.wecenter.com/article/" class="nckmore">更多&gt;&gt;</a>
               </div>
 
               <div class="nzllist clearfix">
                 <dl>
                   <dt><a href="http://wenda.wecenter.com/article/1687"><img
-                    src="http://wenda.wecenter.com/static/css/default/img/default-cover.jpg"></a></dt>
+                    src="@/assets/css/default/img/default-cover.jpg"></a></dt>
                   <dd>
                     <h2><a href="http://wenda.wecenter.com/article/1687">wc的成功案例有哪些</a></h2>
                     <b>朱大老</b>
                     <span>
-                                            <em><i><img
-                                              src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 2</em>
-                                            <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 327</em>
-                                            <em class="ntime">2019-08-14 23:41</em>
-                                        </span>
-                  </dd>
-                </dl>
-                <dl style="position: relative; left: 17px;">
-                  <dt><a href="http://wenda.wecenter.com/article/1683"><img
-                    src="http://wenda.wecenter.com/static/css/default/img/default-cover.jpg"></a></dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/article/1683">文章逻辑删除后，统计数量出错！</a></h2>
-                    <b>程_什么么</b>
-                    <span>
-                                            <em><i><img
-                                              src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                                            <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 158</em>
-                                            <em class="ntime">2019-08-10 19:35</em>
-                                        </span>
-                  </dd>
-                </dl>
-                <dl style="float: right;">
-                  <dt><a href="http://wenda.wecenter.com/article/1682"><img
-                    src="http://wenda.wecenter.com/static/css/default/img/default-cover.jpg"></a></dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/article/1682">动态 数据数量问题</a></h2>
-                    <b>程_什么么</b>
-                    <span>
-                                            <em><i><img
-                                              src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 2</em>
-                                            <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 171</em>
-                                            <em class="ntime">2019-08-08 02:40</em>
-                                        </span>
-                  </dd>
-                </dl>
-                <dl>
-                  <dt><a href="http://wenda.wecenter.com/article/1681"><img
-                    src="http://wenda.wecenter.com/static/css/default/img/default-cover.jpg"></a></dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/article/1681">wangEditor文本编辑器测试一下表情包</a></h2>
-                    <b>果冻汁</b>
-                    <span>
-                                            <em><i><img
-                                              src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                                            <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 206</em>
-                                            <em class="ntime">2019-08-05 09:57</em>
-                                        </span>
-                  </dd>
-                </dl>
-                <dl style="position: relative; left: 17px;">
-                  <dt><a href="http://wenda.wecenter.com/article/1680"><img
-                    src="http://wenda.wecenter.com/static/css/default/img/default-cover.jpg"></a></dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/article/1680">近期QQ群内出现有南通人叫石磊的骗子大家留意谨防被骗</a></h2>
-                    <b>格维先生</b>
-                    <span>
-                                            <em><i><img
-                                              src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 0</em>
-                                            <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 288</em>
-                                            <em class="ntime">2019-07-29 17:13</em>
-                                        </span>
-                  </dd>
-                </dl>
-                <dl style="float: right;">
-                  <dt><a href="http://wenda.wecenter.com/article/1679"><img
-                    src="http://wenda.wecenter.com/static/css/default/img/default-cover.jpg"></a></dt>
-                  <dd>
-                    <h2><a href="http://wenda.wecenter.com/article/1679">还记得老夫的吹牛贴么，一个月增粉1万3.</a></h2>
-                    <b>三叔</b>
-                    <span>
-                                            <em><i><img
-                                              src="http://wenda.wecenter.com/static/css/default/img/nz.png"></i> 1</em>
-                                            <em><i><img src="http://wenda.wecenter.com/static/css/default/img/nsee.png"></i> 338</em>
-                                            <em class="ntime">2019-07-24 10:20</em>
-                                        </span>
+                        <em><i><img
+                          src="@/assets/css/default/img/nz.png"></i> 2</em>
+                        <em><i><img src="@/assets/css/default/img/nsee.png"></i> 327</em>
+                        <em class="ntime">2019-08-14 23:41</em>
+                    </span>
                   </dd>
                 </dl>
               </div>
               <h6 class="nhref" id="article_all_more" data-page="1"><i><img
-                src="http://wenda.wecenter.com/static/css/default/img/href.png"></i>换一换</h6>
+                src="@/assets/css/default/img/href.png"></i>换一换</h6>
             </div>
           </div>
           <div class="N-right clearfix">
@@ -283,11 +97,11 @@
                   <ul>
                     <li>
                       <img src="@/assets/css/default/img/nsee.png">
-                      文章总浏览量:<i>{{column.votes_count}}</i>
+                      文章总浏览量:<i>{{column.views_count}}</i>
                     </li>
                     <li>
                       <img src="@/assets/css/default/img/nnum.png">
-                      发表文章总数:<i>{{column.views_count}}</i>
+                      发表文章总数:<i>{{column.article_count}}</i>
                     </li>
                   </ul>
                 </div>
