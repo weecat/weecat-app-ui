ca65db92a8e1c8f58eef526712b1e8961eb5aac8
wangkun23 <845885222@qq.com>
2019-09-11 22:25:37 +0800

    模拟数据增加

--- src/mock/api/v1/user/UserQuestionsMock.js
+++ src/mock/api/v1/user/UserQuestionsMock.js
@@ -8,13 +8,12 @@ Mock.mock('/api/v1/user/questions/list', 'post', {
     data: {
       'list|0-10': [{
         'id': '@id',
-        'title|3-7': '@cname',
+        'title|5-30': '@cname',
         'link': '',
         'question_info': {
           'answer_count|1-100': 100,
           'view_count|1-100': 100,
           'focus_count|1-100': 100,
-          'answer_content|1-130': '@cname',
           'add_time': '@now'
         }
       }]
